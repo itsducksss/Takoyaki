a21ee76d6d0b90a2c0ffc2df188b6397e67b1b9c
itsducksss <152821176+itsducksss@users.noreply.github.com>
2025-11-20 16:16:36 +0000

    Fixed Water and TMP + Scene Reset
    Water does now work with the slider going up when touching an object tagged with "Water", the value does not exceed 100f even after staying there for extended periods of time, and continues to decrease in slider/health when not touching the tagged object. So far only works on the torso/ object attached onto the script with a collider.
    
    The text mesh pro is now fixed showing the value of the slider not showing backwards anymore and value correlates with the value of the slider.
    
    The scene now resets using scene manager in unity when the slider value reaches 0f.

--- Assets/healthManager.cs
+++ Assets/healthManager.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-using static UnityEngine.UIElements.UxmlAttributeDescription;
+using UnityEngine.SceneManagement;
 
-public class healthManager : MonoBehaviour
+public class HealthManager : MonoBehaviour
 {
     public Slider healthBar;
     public float healthAmount = 100f;
@@ -15,32 +15,52 @@ public class healthManager : MonoBehaviour
     private Vector3 center;
     private float radius;
 
+    public bool isWatered;
+
     public TMP_Text healthText;
 
     [SerializeField] Transform _groundPoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
-    {
+    {                               
         hitColliders = new Collider[maxColliders];
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthAmount -= coef * 0.5f; //(Change back to 0.05)
+                if (isWatered == true)
+            {
+                healthAmount += coef * 0.5f;
+                Debug.Log("Gulp...gulp...gulp");
+
+                if (healthAmount > 100f)
+                {
+                    healthAmount = 100f;
+                    Debug.Log("Drowwnning in so much water");
+            }
+        }
+        else
+        {
+            Debug.Log("THOIUSTRY WAAAWWTER");
+            healthAmount -= coef * 0.05f;
+        }
+
+
+        //healthAmount -= coef * 0.05f; //(Change back to 0.05)
         healthBar.value = (float)healthAmount / 100f;
 
         healthText.text = healthAmount + "/" + 100;
 
         if (healthAmount <= 0)
-        { 
-            healthBar.gameObject.SetActive(false);
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        //if (Input.GetKeyDown(KeyCode.Return))
-        //{
-        //    TakeDamage(25);
-        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(25);
+        }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
@@ -49,18 +69,30 @@ public class healthManager : MonoBehaviour
     }
 
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.tag == "Water")
-    //    {
-    //        healthAmount = healthAmount + 25;
-    //    }
-    //}
-    //public void TakeDamage(float damage)
-    //{ 
-    //    healthAmount -= damage;
-    //    healthBar.value = healthAmount / 100f;
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Water")
+        {
+            isWatered = true;
+            //healthAmount = healthAmount + 25;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Water")
+        {
+            isWatered = false;
+            //healthAmount = healthAmount + 25;
+        }
+
+    }
+    public void TakeDamage(float damage)
+    { 
+        healthAmount -= damage;
+        healthBar.value = healthAmount / 100f;
+    }
 
 
 
