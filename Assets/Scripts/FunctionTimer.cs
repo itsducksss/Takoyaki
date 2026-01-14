879bd9e5d983c25d1dc26d8e267e597e2faa8f2e
itsducksss <152821176+itsducksss@users.noreply.github.com>
2026-01-14 20:54:12 +0000

    Breakable puzzle + Kenneys Assets

--- Assets/Scripts/FunctionTimer.cs
+++ Assets/Scripts/FunctionTimer.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class FunctionTimer
 {
+    public static FunctionTimer Create(Action action, float timer) 
+    {
+        FunctionTimer functionTimer = new FunctionTimer(action, timer);
+
+        GameObject gameObject = new GameObject("FunctionTimer", typeof(MonoBehaviourHook));
+        gameObject.GetComponent<MonoBehaviourHook>().onUpdate = functionTimer.Update;
+
+        return functionTimer;
+    }
+    //Dummy class to have access to MonoBehaviour functions
+    private class MonoBehaviourHook : MonoBehaviour
+    {
+        public Action onUpdate;
+            private void Update()
+        { 
+            if(onUpdate != null) onUpdate();
+        }
+    }
     private Action action;
     private float timer;
+    private bool isDestroyed;
 
-    public FunctionTimer(Action action, float timer)
+    private FunctionTimer(Action action, float timer)
     {
         this.action = action;
         this.timer = timer;
+        isDestroyed = false;
     }
 
     public void Update()
     {
-    timer -= Time.deltaTime;
-        if (timer < 0)
-        { 
-        action();
+        if (!isDestroyed)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                action();
+                DestroySelf();
+            }
         }
+    }
+
+    private void DestroySelf()
+    { 
+        isDestroyed = true;
     }
 }
